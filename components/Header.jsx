3f5225ec7f89cde442191930ac8740862f3e7925
sumandul <dulalsuman853@gmail.com>
2023-09-27 22:12:38 +0545

    update dark-mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ export function AppHeader() {
 						{isMobile ? <MobileMenu /> : <Menu />}
 					<div className="flex items-center gap-5">
 						{!isMobile &&<div className="  absolute right-1 top-[20rem]"> <ConnectMedia /> </div>}
-						{/* <ThemeSwitcher /> */}
+						<ThemeSwitcher />
 					</div>
 				</div>
 				</div>
